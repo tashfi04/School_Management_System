0b8112d6e2e1bc786103dc5dc4b8a05317b3e933
Tashfi <mohammad11@student.sust.edu>
2020-04-21 20:47:02 +0600

    Configured Django with Postgresql database

--- back_end/back_end/settings.py
+++ back_end/back_end/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'back_end.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'schoolDatabase',
+        'USER': 'admin',
+        'PASSWORD': '0112358',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
