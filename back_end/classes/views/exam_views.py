d0d4effe35e705ef16950b3aa0afeca08f80c2ca
Emon(2017831003) <43314141+emon-swe-sust@users.noreply.github.com>
2021-01-20 17:20:00 +0600

    tabulation sheet exam pk error resolved

--- back_end/classes/views/exam_views.py
+++ back_end/classes/views/exam_views.py
@@ -1,3 +1,4 @@
+from ..models import Exam
 from exams.models import ExamType
 from rest_framework import permissions
 
@@ -9,7 +10,8 @@ from rest_framework.generics import (
 )
 
 from ..serializers import (
-    ClassExamTypeListSerializer
+    ClassExamTypeListSerializer,
+    SubjectExamListSerializer
 )
 
 class ClassExamTypeList(ListAPIView):
@@ -22,6 +24,22 @@ class ClassExamTypeList(ListAPIView):
         class_id = self.kwargs.get('class_pk', None)
 
         queryset = ExamType.objects.filter(exam__related_class_id=class_id).distinct()
+
+        if queryset:
+            return queryset
+        else:
+            raise NotFound("No exam has been added to this class yet!")
+
+class SubjectExamList(ListAPIView):
+
+    permission_classes = [permissions.AllowAny]
+    serializer_class = SubjectExamListSerializer
+
+    def get_queryset(self):
+
+        subject_pk = self.kwargs.get('subject_pk', None)
+
+        queryset = Exam.objects.filter(subject_id=subject_pk).distinct()
 
         if queryset:
             return queryset
