a189166ada02e222e270111959bc9e3805e0adbd
Tahseen Rasheed Chowdhury <35876394+TahseenSust@users.noreply.github.com>
2021-02-07 15:37:59 +0600

    fixed errors

--- routine/file/routine.py
+++ routine/file/routine.py
@@ -1,63 +1,75 @@
-teacher_queue = []
-routine = []
+class RoutineGenerator():
 
-def generateRoutine(class_description, teacher_list, max_class_a_day):
-    
-    generateWeek(class_description, teacher_list, max_class_a_day)
-    
-    for items in class_description:
-        printWeekForClass()
-        
-        
-def generateWeek(class_description, teacher_list, max_class_a_day):
-    
-    for day in range(5):
-        generateTeacherQueue(class_description, teacher_list, max_class_a_day)
-        generateForSingleDay(day, class_description, teacher_list, max_class_a_day)
-        
-        
-def generateTeacherQueue(class_description, teacher_list, max_class_a_day):
-    
-    for indx, classes in enumerate(class_description):
-        
-        for j, subjects in enumerate (classes.subject_set):
-            teacher_queue[index].append(j)
-    
-    
-def generateForSingleDay(day, class_description, teacher_list, max_class_a_day):
+    teacher_queue = [[0, 0], [0, 0]]
 
-    count_class = []
-    for gap in max_class_a_day:
-        
-        for a, _class in enumerate(class_description):
+    routine_list = []
+
+    routine_row = {
+        'related_class_id': None,
+        'period': None,
+        'sunday': None,
+        'monday': None,
+        'tuesday': None,
+        'wednesday': None,
+        'thursday': None
+    }
+
+
+    def generateForSingleDay(self, day, class_description, teacher_list, max_class_a_day):
+
+        count_class = []
+        for gap in range(max_class_a_day):
             
-            if routine[day][a].size() != _class.total_class_a_day
+            for a, _class in enumerate(class_description):
+                
+                if len(self.routine[day][a]) != _class.total_class_in_a_day:
 
-                availableTeacher = []
-                canGenerate = False
-                for teacher in teacher_list:
-                    
-                    i = teacher_queue.pop(0)
+                    availableTeacher = []
+                    canGenerate = False
 
-                    if (not (availableTeacher[_class.subject_set[i]['teacher_id']])) and (_class.subject_set[i]['total_class_a_week'] > count_class[a][i]):
+                    for teacher in teacher_list:
                         
-                        availableTeacher[_class.subject_set[i]['teacher_id']] = True
-                        """
-                        fix the code below
-                        """
-                        #_class[i].total_class_a_week -= 1
+                        i = self.teacher_queue[a].pop(0)
 
-                        count_class[a][i] += 1
+                        if (not (availableTeacher[_class.subject_set.all()[i]['teacher_id']])) and (_class.subject_set.all()[i]['total_class_a_week'] > count_class[a][i]):
+                            
+                            availableTeacher[_class.subject_set.all()[i]['teacher_id']] = True
+                            """
+                            fix the code below
+                            """
+                            #_class[i].total_class_a_week -= 1
+
+                            count_class[a][i] += 1
+                            
+                            self.routine[day][a].push_back({_class.subject_set.all()[i].id, _class.subject_set.all()[i].teacher_id})
+                            
+                            self.teacher_queue[a].appnd(i)
+                            canGenerate = True
+                            break
                         
-                        routine[day][a].push_back({_class.subject_set[i].id, _class.subject_set[i].teacher_id})
+                        self.teacher_queue[a].appnd(i)
                         
-                        teacher_queue[a].appnd(i)
-                        canGenerate = True
-                        break
-                    
-                    teacher_queue[a].appnd(i)
-                    
-                if not canGenerate:
-                    print("Can't generate routine!!!")
-                    
-                    return None
+                    if not canGenerate:
+                        print("Can't generate routine!!!")
+                        
+                        return None
+
+
+    def generateTeacherQueue(self, class_description, teacher_list, max_class_a_day):
+        
+        for index, classes in enumerate(class_description):
+            
+            for j, subjects in enumerate (classes.subject_set.all()):
+                self.teacher_queue[index].append(j)
+
+    def generateWeek(self, class_description, teacher_list, max_class_a_day):
+        
+        for day in range(5):
+            self.generateTeacherQueue(class_description, teacher_list, max_class_a_day)
+            self.generateForSingleDay(day, class_description, teacher_list, max_class_a_day)
+
+    def generateRoutine(self, class_description, teacher_list, max_class_a_day):
+        
+        self.generateWeek(class_description, teacher_list, max_class_a_day)
+            
+        
